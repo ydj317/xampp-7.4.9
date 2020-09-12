dd53abb69b5555a63102200c3864aab8046f7a5a
ydj317 <ydj317@163.com>
2020-09-12 13:31:02 +0800

    update

--- docker-build-origin.sh
+++ docker-build-origin.sh
@@ -3,4 +3,4 @@
 clear
 
 echo "Start Build Dockker":
-docker build -t ydj317/xampp-7.4.9:origin .
+docker build -t ydj317/xampp-7.4.9:version-1.0.1 .
--- docker-run-origin.sh
+++ docker-run-origin.sh
@@ -3,5 +3,5 @@
 clear
 
 echo "Start Lampp Docker Run"
-docker run -i -t --name lampp -v ~/lampp:/opt/lampp -p 80:80 -p 443:443 -p 3306:3306 -d ydj317/xampp-7.4.9:origin
+docker run -i -t --name lampp -v ~/lampp:/opt/lampp -p 80:80 -p 443:443 -p 3306:3306 -d ydj317/xampp-7.4.9:version-1.0.1
 echo "OK"
